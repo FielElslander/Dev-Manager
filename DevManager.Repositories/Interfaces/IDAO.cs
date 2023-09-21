cac3b36421ba6abe66f9ed88736bd4c13e753a6f
Fiel Elslander <fiel.elslander@student.vives.be>
2023-09-21 22:24:06 +0200

    Interfaces added

---
+++ DevManager.Repositories/Interfaces/IDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevManager.Repositories.Interfaces
+{
+    public interface IDAO<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task Add(T entity);
+        Task Delete(T entity);
+        Task Update(T entity);
+        Task<T> FindById(int id);
+    }
+}
